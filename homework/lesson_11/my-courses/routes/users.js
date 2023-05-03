b72ecb129f594dc31efbe073f005da137d61d187
Ostrovskiy Konstantin <web@ostkost.ru>
2023-05-03 22:14:22 +0300

    Move lesson 29 to root

--- homework/lesson_11/my-courses/routes/users.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as controller from '../controllers/users.js';
-
-export const router = express.Router({ mergeParams: true });
-
-router
-  .route('/:id')
-  .get(controller.getUser)
-  .put(controller.updateUser);
